20f677b51c2ccfb05a1f553f743e72dfa019612a
Diwakar <ydr4urs@gmail.com>
2020-06-16 17:48:13 +0530

    updated database
    updated database  username and password

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'emifound_emifoundationmain');
 
 /** MySQL database username */
-define('DB_USER', 'emifound_emifoun');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'EmiFoundation@2018');
+define('DB_PASSWORD', 'InterAKT@Emi');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
